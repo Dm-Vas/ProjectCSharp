1c4dca378d38094356fe96140f82cc8e6758351a
DV <vdletter@gmail.com>
2022-09-17 22:41:15 -0700

    Create UniverseWindow, add functionality for adding new universes

---
+++ SuperheroLog/UniverseWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using SuperheroLog.Database;
+using System.Windows;
+
+namespace SuperheroLog
+{
+    /// <summary>
+    /// Interaction logic for UniverseWindow.xaml
+    /// </summary>
+    public partial class UniverseWindow : Window
+    {
+        public UniverseWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUniverseName.Text.Trim() == "")
+                MessageBox.Show("Universe Name cannot be empty");
+            else
+            {
+                using (SUPERHEROBASEContext database = new SUPERHEROBASEContext())
+                {
+                    Universe universe = new();
+                    universe.UniverseName = txtUniverseName.Text;
+                    database.Universes.Add(universe);
+                    database.SaveChanges();
+                    txtUniverseName.Clear();
+                    MessageBox.Show("New Universe was added succesfully");
+                }
+            }
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- SuperheroLog/Views/UniverseList.xaml.cs
+++ SuperheroLog/Views/UniverseList.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using SuperheroLog.Database;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace SuperheroLog.Views
@@ -16,6 +17,17 @@ namespace SuperheroLog.Views
             using SUPERHEROBASEContext databse = new();
             List<Universe> list = databse.Universes.OrderBy(univerce => univerce.UniverseName).ToList();
             gridUniverse.ItemsSource = list;
+        }
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            UniverseWindow window = new();
+            this.Visibility = Visibility.Collapsed;
+            window.ShowDialog();
+            using SUPERHEROBASEContext database = new();
+            List<Universe> list = database.Universes.OrderBy(universe => universe.UniverseName).ToList();
+            gridUniverse.ItemsSource = list;
+            this.Visibility = Visibility.Visible;
         }
     }
 }
