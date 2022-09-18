c5195316c9d682f30227d650852e5a53658809d5
DV <vdletter@gmail.com>
2022-09-17 22:03:39 -0700

    Create Universe model and user control

---
+++ SuperheroLog/Views/UniverseList.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SuperheroLog.Database;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace SuperheroLog.Views
+{
+    /// <summary>
+    /// Interaction logic for UniverseList.xaml
+    /// </summary>
+    public partial class UniverseList : UserControl
+    {
+        public UniverseList()
+        {
+            InitializeComponent();
+            using SUPERHEROBASEContext databse = new();
+            List<Universe> list = databse.Universes.OrderBy(univerce => univerce.UniverseName).ToList();
+            gridUniverse.ItemsSource = list;
+        }
+    }
+}
